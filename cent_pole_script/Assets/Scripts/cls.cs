64f67547e49ab698848f5a8821dc0b9ce4af1e05
lexa2907 <lesha.eu1@mail.ru>
2020-12-27 21:19:36 +0500

    центральное поле - скрипт

---
+++ cent_pole_script/Assets/Scripts/cls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cls : MonoBehaviour
+{
+    public float v;
+    private float p;
+    private float e;
+    private float g = 9.8f;
+    private float t = 0;
+
+
+    void Start()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        e = Mathf.Sqrt(1 + Mathf.Pow(v * Mathf.Cos(Mathf.Pow(Mathf.PI,2) / 360) / (g * Mathf.Sqrt(x * x + y * y)), 2) * (v * v - 2 * g * Mathf.Sqrt(x * x + y * y)));
+        p = Mathf.Pow(v * Mathf.Cos(Mathf.Pow(Mathf.PI,2) / 360),2) / g;
+    }
+
+
+    
+    void Update()
+    {
+        t += Time.deltaTime;
+        transform.position = new Vector3((p / (1 + e * Mathf.Cos(t))) * Mathf.Cos(t), (p / (1 + e * Mathf.Cos(t))) * Mathf.Sin(t), 0);
+    }
+}
